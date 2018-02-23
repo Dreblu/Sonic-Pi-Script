f95300b1efbec3264d558212d7f9b8e04c26bc3a
Dreblu <andre_alexander@davincischools.org>
2018-02-23 13:45:52 -0800

    Update dice.rb

--- dice.rb
+++ dice.rb
@@ -9,13 +9,13 @@
 # Stranger Things Main Theme
 use_bpm 160
 use_synth :saw
-notes = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]
+notes = [:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2] #notes that will play 
 
 
 live_loop :strategy1 do
   # assign a value to 'a' using the .choose method from the Randomisation reading on Tuesday
-  a = choose ([:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2])
-  print a
+  a = choose ([:c2, :e2, :g2, :b2, :c3, :b2, :g2, :e2]) #with this method it will pick a random note and play it
+  print a #The print feature is just when the random note is selected it will show on the right side where the music waves are. (numbers)
   play a
   sleep 0.5
 end
@@ -23,16 +23,16 @@ end
 
 live_loop :strategy2 do
   # assign a value to 'b' that will be used as the index value
-  b = rrand_i(0, 7)
+  b = rrand_i(0, 7) #the b is being selested between the number 0 and 7, nothing more nothing less
   print b
-  play notes[b]
+  play notes[b] #this plays only the b note.
   sleep 0.5
 end
 
 
 live_loop :strategy3 do
   # assign a value to 'c' that will be used as the index value
-  c = dice(8)-1
+  c = dice(8)-1 #this dice method is when the number in parenthesis is being doubled to increase sound or decrease (not exactly sure)
   print c
   play notes[c]
   sleep 0.5
